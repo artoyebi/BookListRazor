8fac6786bbe32f6b9623aa2d6af0075b41302432
Olaoluwa Atoyebi <artoyebi@gmail.com>
2020-04-03 23:34:15 +0100

    Initial commit

---
+++ BookListRazor/Migrations/20200520125904_AddYearToBookModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookListRazor.Migrations
+{
+    public partial class AddYearToBookModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Year",
+                table: "Book",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Year",
+                table: "Book");
+        }
+    }
+}
